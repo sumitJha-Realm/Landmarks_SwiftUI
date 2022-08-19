d8d044b23fa9e5788acb0dc336fec5f296ce5b76
Sumit Jha <72190898+sumitJha-Realm@users.noreply.github.com>
2022-08-19 19:15:02 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sumit Jha on 19/08/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
